8f70f67e95e7de0699d166d4fc362fb648f5f3ab
farsider350 <31876349+farsider350@users.noreply.github.com>
2024-04-02 19:44:48 +1000

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,34 @@
+let config = {
+  myself: [
+    '1MzFr1eKzLEC1',
+    'LSRfZJf75Mtwz3'
+  ],
+  host: 'http://ltcmint.autradex.systems:9327/',
+  // data reload interval in seconds
+  reload_interval: 30,
+  // chart reload interval in seconds
+  reload_chart_interval: 600,
+  // HTML to load at top of page
+  header_content_url: 'http://localhost:8888/custom_content.html',
+  // Default Theme
+  theme: 'cyborg',
+  // Available Themes
+  available_themes: [
+    'default',
+    'amelia',
+    'cerulean',
+    'cosmo',
+    'cyborg',
+    'darkly',
+    'flatly',
+    'journal',
+    'lumen',
+    'readable',
+    'simplex',
+    'slate',
+    'spacelab',
+    'superhero',
+    'united',
+    'yeti'
+  ]
+}
